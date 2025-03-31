234f7b1848e66676903f39d8e561549d7cc1d595
SandwichGouda <william.driot@gmail.com>
2025-03-31 15:15:33 +0200

    Different elements with different colors

--- 3 - Frontend JavaScript/exercise1.js
+++ 3 - Frontend JavaScript/exercise1.js
@@ -14,3 +14,22 @@ function loadDoc(event) {
 let btn = document.getElementById("b1");
 btn.addEventListener("click", loadDoc);
 
+function loadDoc2() {
+    fetch("text.txt")
+    .then(response => response.text())
+    .then(text => {
+        // let t = text.split("<br/>\n");
+        let t = text.split("\n");
+        let colors = ["red","green","blue","purple","orange","gray"]
+        let div = document.getElementById("tarea2");
+        for (let k in t) {
+            let p = document.createElement("p");
+            p.setAttribute("style","color : "+colors[k]+"\n");
+            p.textContent = t[k];
+            div.appendChild(p)
+        }
+    })
+}
+
+let btn2 = document.getElementById("b2");
+btn2.addEventListener("click", loadDoc2);
